b3c11be12a1882b9d6656a02753460f93eddd7c0
mikaelpackowski <mikael.packowski@gmail.com>
2025-08-11 21:11:31 +0200

    Create Separator.tsx

---
+++ components/ui/Separator.tsx
@@ -0,0 +1,7 @@
+export default function Separator() {
+  return (
+    <div className="mx-auto max-w-6xl px-4 sm:px-6">
+      <div className="border-t border-gray-200 py-12 md:py-16" />
+    </div>
+  );
+}
