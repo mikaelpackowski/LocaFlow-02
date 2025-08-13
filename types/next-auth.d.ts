cdd4938728a24b73affa5e470cf5080e9e647ab1
mikaelpackowski <mikael.packowski@gmail.com>
2025-08-13 10:31:22 +0200

    Create next-auth.d.ts

---
+++ types/next-auth.d.ts
@@ -0,0 +1,16 @@
+// next-auth.d.ts
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    role?: "owner" | "tenant";
+  }
+  interface Session {
+    user: {
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      role?: "owner" | "tenant";
+    };
+  }
+}
